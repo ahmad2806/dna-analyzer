75ba726581c95a866745fae3866a9ba98894111a
ahmad2806 <ahmadhashem2806@gmail.com>
2018-12-30 11:46:40 +0200

    adding a defualt function to command that all commands uses to print

--- model/Command.h
+++ model/Command.h
@@ -6,9 +6,13 @@
 #define PROJECT_DNA_ANALYZER_COMMAND_H
 
 #include <iostream>
+#include <sstream>
 #include <vector>
+#include "dna_code/DNA/DnaSquence.h"
+
 class Command{
 public:
+    std::string build_return_value (unsigned int _id, const std::string &_name, const DnaSequence* const _sequence);
     virtual std::string run_command(const std::vector<std::string> &vector) = 0;
     virtual ~Command(){};
 };
