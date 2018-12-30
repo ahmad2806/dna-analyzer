f0d1065fdbfc8c0b7efc3949611367165b316995
ahmad2806 <ahmadhashem2806@gmail.com>
2018-12-30 11:49:35 +0200

    use sharedptr on creating new commands

--- model/CommandHandler.cpp
+++ model/CommandHandler.cpp
@@ -2,16 +2,19 @@
 // Created by ahmad on 29/12/2018.
 //
 #include "CommandHandler.h"
+#include "../controller/sharedPtr/SharedPtr.h"
 
 std::string CommandHandler::create_and_run_command(const std::vector<std::string> &_vector) const{
     Command *local_cmd = command_factory(_vector.at(0));
+    SharedPtr<Command> m_command(local_cmd);
+
     std::string run_result;
     if (!local_cmd)
         throw std::invalid_argument("no such command please try again");
 
-    run_result = local_cmd->run_command(_vector);
+    run_result = m_command.get()->run_command(_vector);
 
-    delete local_cmd;
+
     return run_result;
 }
 
